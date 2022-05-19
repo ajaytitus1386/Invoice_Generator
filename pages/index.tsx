8d68cf81f0668f441a328bd8577187ea56c474dd
Ajay Titus <ajaytitus1386@gmail.com>
2022-05-19 23:47:16 +0530

    feat: add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import LandingHeader from "../components/Landing/LandingHeader";
 import SavedInvoices from "../components/Landing/SavedInvoices";
@@ -6,6 +7,9 @@ import SavedInvoices from "../components/Landing/SavedInvoices";
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>Invoice Generator</title>
+      </Head>
       <div className="absolute right-32 top-12">
         <Image src="/assets/invoice_image.png" width={420} height={460} />
       </div>
