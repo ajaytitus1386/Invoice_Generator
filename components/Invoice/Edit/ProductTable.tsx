1cd9ad0ff3ca23becd3e9ed3f808d16d6220634b
Ajay Titus <ajaytitus1386@gmail.com>
2022-05-20 02:29:09 +0530

    feat: table of products

---
+++ components/Invoice/Edit/ProductTable.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { Invoice } from "../../../models/invoice";
+import { Product } from "../../../models/product";
+import getProducts from "../../../services/product/getProducts";
+import ProductTableRow from "./ProductTableRow";
+
+interface ProductTableProps {
+  invoice: Invoice;
+  setInvoice: React.Dispatch<React.SetStateAction<Invoice>>;
+}
+
+function ProductTable({ invoice, setInvoice }: ProductTableProps) {
+  const [productsMenuItems, setProductsMenuItems] = useState<Product[]>([]);
+  async function fetchProductsFromApi() {
+    const data = await getProducts();
+    setProductsMenuItems(data);
+  }
+  useEffect(() => {
+    fetchProductsFromApi();
+  }, []);
+
+  const latestProduct: Product = {
+    id: invoice.products.length,
+    description: "",
+    quantity: 0,
+    rate: 0,
+  };
+  return (
+    <div className="p-4 border-2 border-solid border-midPurple rounded-xl">
+      <table className="w-full table-fixed">
+        <thead className="w-full text-blueMarguerite">
+          <tr className="">
+            <th className="w-3/5 px-1 text-left">Description</th>
+            <th className="px-1 text-center">Rate</th>
+            <th className="px-1 text-center">Qty</th>
+            <th className="px-1 text-center">Line Total</th>
+          </tr>
+        </thead>
+
+        <tbody className="">
+          <ProductTableRow
+            product={latestProduct}
+            productsMenuItems={productsMenuItems}
+            setProductsMenuItems={setProductsMenuItems}
+            index={invoice.products.length}
+            invoice={invoice}
+            setInvoice={setInvoice}
+          />
+          {invoice.products &&
+            invoice.products.map((product, index) => {
+              return (
+                <ProductTableRow
+                  key={index}
+                  index={invoice.products.length}
+                  productsMenuItems={productsMenuItems}
+                  setProductsMenuItems={setProductsMenuItems}
+                  product={product}
+                  invoice={invoice}
+                  setInvoice={setInvoice}
+                />
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ProductTable;
