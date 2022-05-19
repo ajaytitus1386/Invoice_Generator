af4b6d96828f5b5e4ec26bfe54ba456bee88f168
Ajay Titus <ajaytitus1386@gmail.com>
2022-05-19 23:41:56 +0530

    feat: navbar title hidden on landing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,24 @@
 import Image from "next/image";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 function Navbar() {
+  const router = useRouter();
+  const { pathname } = router;
+
+  const [isLandingPage, setIsLandingPage] = useState(pathname == "/");
+  useEffect(() => {
+    setIsLandingPage(pathname == "/");
+  }, [router]);
+
   return (
-    <div className="sticky top-0 z-10 flex flex-row items-center justify-start space-x-2 w-full px-safe py-4 h-18 md:h-24 bg-white">
+    <div className="sticky top-0 z-10 flex flex-row items-center justify-start w-full py-4 space-x-2 bg-white px-safe h-18 md:h-24">
       <Image src={"/assets/logo_purple.png"} width={29} height={29} />
-      <h1 className="text-2xl font-extrabold text-blueMarguerite">
-        Invoice Generator
-      </h1>
+      {!isLandingPage && (
+        <h1 className="text-2xl font-extrabold text-blueMarguerite">
+          Invoice Generator
+        </h1>
+      )}
     </div>
   );
 }
